1df19a7592461e49e6b2bb1b86caa151eb38608f
Abdüllahy Bashir <abdullahi.b@protonmail.com>
2025-02-13 12:42:47 +0300

    more methods for string manipulation

--- Python/Lessons/finding_replacing_strings.py
+++ Python/Lessons/finding_replacing_strings.py
@@ -11,3 +11,15 @@ try:
     print(my_name.index("hey"))
 except ValueError:
     print("Substring not found")
+
+# counting occurences
+# we use the .count() method to count the number of times a substring appears in a target string.
+# syntax: string.count(substring, start, end) "start" and "end" are optional attributes.
+name2 = "My name is Abdullahi Bashir"
+print(name2.count("a", 0, 7))
+
+# replacing substrings
+# we use the .replace() method to replace a substring with another substring.
+# syntax: string.replace(old_substring, new_substring, count) "count" is an optional attribute.
+name3 = "My name is Abdullahi Bashir"
+print(name3.replace("Abdullahi", "Abdullahy"))
